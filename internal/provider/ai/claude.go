f91927450c4482b3d1a7c6d380e4d6fb320471cb
Slava Mihaelov <huskyslava@gmail.com>
2026-01-13 16:02:59 +0200

    Cluade - provider implemenation - wip

--- internal/provider/ai/claude.go
+++ internal/provider/ai/claude.go
@@ -3,6 +3,7 @@ package ai
 import (
 	"ai/internal/config"
 	"context"
+	"fmt"
 	"net/http"
 	"time"
 )
@@ -27,19 +28,25 @@ func NewClaude(model string) (*ClaudeProvider, error) {
 }
 
 func (p *ClaudeProvider) Rewrite(ctx context.Context, text string) (string, error) {
-	return "", nil
+	prompt := p.cfg.Prompts.Rewrite + " " + text
+	return p.SendRequest(ctx, prompt)
 }
 
 func (p *ClaudeProvider) Translate(ctx context.Context, text string, toLanguage string) (string, error) {
-	return "", nil
+	prompt := fmt.Sprintf(p.cfg.Prompts.Translate, toLanguage) // Inject target language to config prompt
+	prompt += " " + text                                       // Add text to be translated
+	return p.SendRequest(ctx, prompt)
 }
 
 func (p *ClaudeProvider) Summarize(ctx context.Context, text string) (string, error) {
-	return "", nil
+	prompt := fmt.Sprintf(p.cfg.Prompts.Summarize)
+	prompt += " " + text
+	return p.SendRequest(ctx, prompt)
 }
 
 func (p *ClaudeProvider) General(ctx context.Context, text string) (string, error) {
-	return "", nil
+	prompt := text
+	return p.SendRequest(ctx, prompt)
 }
 
 func (p *ClaudeProvider) SendRequest(ctx context.Context, prompt string) (string, error) {
