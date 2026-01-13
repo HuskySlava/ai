7e7b8a98d1d2c2658585ef326da5b50721d173ac
Slava Mihaelov <huskyslava@gmail.com>
2026-01-13 16:01:25 +0200

    Cluade - provider implemenation - wip

--- internal/provider/ai/claude.go
+++ internal/provider/ai/claude.go
@@ -2,6 +2,7 @@ package ai
 
 import (
 	"ai/internal/config"
+	"context"
 	"net/http"
 	"time"
 )
@@ -23,4 +24,24 @@ func NewClaude(model string) (*ClaudeProvider, error) {
 		client: &http.Client{Timeout: time.Duration(cfg.HttpTimeoutSeconds) * time.Second},
 		cfg:    cfg,
 	}, nil
+}
+
+func (p *ClaudeProvider) Rewrite(ctx context.Context, text string) (string, error) {
+	return "", nil
+}
+
+func (p *ClaudeProvider) Translate(ctx context.Context, text string, toLanguage string) (string, error) {
+	return "", nil
+}
+
+func (p *ClaudeProvider) Summarize(ctx context.Context, text string) (string, error) {
+	return "", nil
+}
+
+func (p *ClaudeProvider) General(ctx context.Context, text string) (string, error) {
+	return "", nil
+}
+
+func (p *ClaudeProvider) SendRequest(ctx context.Context, prompt string) (string, error) {
+	return "", nil
 }
